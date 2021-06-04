74b8d632791120675db2d41b8b057416bb7ca0c1
markfm <markfm@uvic.ca>
2021-06-04 16:08:44 -0700

    Initial commit!

---
+++ client/index.js
@@ -0,0 +1,31 @@
+/* 1) Create an instance of CSInterface. */
+var csInterface = new CSInterface();
+
+/* 2) Make a reference to your HTML button and add a click handler. */
+var openButton = document.querySelector("#apply-button");
+openButton.addEventListener("click", randomizeFrames);
+
+/* 3) Write a helper function to pass instructions to the ExtendScript side. */
+function randomizeFrames() {
+  
+  const filmIcon1 = document.getElementById("film-icon-1");
+  const filmIcon2 = document.getElementById("film-icon-2");
+  const filmIcon3 = document.getElementById("film-icon-3");
+
+  // Start frame swapping animation
+  filmIcon1.style.animation= "swap-1 0.7s infinite"
+  filmIcon2.style.animation= "swap-2 0.7s infinite"
+  filmIcon3.style.animation= "swap-3 0.7s infinite"
+
+  // 7 is the colour
+  csInterface.evalScript("randomizeFrames(15)");
+
+
+  // End frame swapping animation
+
+  /*
+  filmIcon1.style.animation= "swap-1 0.7s"
+  filmIcon2.style.animation= "swap-2 0.7s"
+  filmIcon3.style.animation= "swap-3 0.7s"
+  */
+}
---
+++ host/index.jsx
@@ -0,0 +1,66 @@
+﻿var project = app.project;
+mainBin = project.rootItem.createBin("Randomized Frames");
+
+function randomizeFrames(labelColour) {
+
+    FRAME_RATE = (1 / project.activeSequence.getSettings().videoFrameRate.seconds).toFixed(3); 
+    theClip = project.activeSequence.getSelection();
+    clipInFrame = secondsToFrames(theClip[0].inPoint.seconds, FRAME_RATE);
+    framesBin = mainBin.createBin("Temp");
+    usedFramesBin = mainBin.createBin(theClip[0].name + " Frames"); 
+    frameCount = secondsToFrames(theClip[0].duration.seconds, FRAME_RATE);
+    
+    for(i = 0; i < frameCount; i++) {
+        startTime = framesToSeconds(i + clipInFrame, FRAME_RATE);
+        endTime = framesToSeconds (i + clipInFrame + 1, FRAME_RATE);
+        clipFrame = theClip[0].projectItem.createSubClip(i + " " + theClip[0].name, startTime, endTime , 0);
+        clipFrame.setColorLabel(labelColour);
+        clipFrame.moveBin(framesBin);
+    }
+    
+    maxFrameCount = frameCount;
+    for(i = 0; i < maxFrameCount; i++) {
+        randIndex = Math.round(Math.random() * frameCount) - 1;
+        if (randIndex < 0) randIndex = 0; 
+        
+        project.activeSequence.videoTracks[0].insertClip(framesBin.children[randIndex], theClip[0].start);
+    
+        framesBin.children[randIndex].moveBin(usedFramesBin);
+        frameCount--;
+    }
+    
+    for (i = 0; i < theClip.length; i++) {
+        if (i < theClip.length - 1) {
+            theClip[i].remove(0 , 0);
+        }
+        else {
+            theClip[i].remove(1 , 0);
+        }
+    }
+    framesBin.deleteBin();
+}
+
+/*
+| theVideo |     
+The ProjectItem video that you want to set the in-point and out-point of.
+
+| inPoint | 
+Input the frame number e.g. 36. Video frame index starts at zero.
+
+| outPoint |
+Input the frame number e.g. 36.
+*/
+function setInAndOut(theVideo, inPoint, outPoint) {
+    theVideo.setInPoint(framesToSeconds(inPoint, FRAME_RATE));
+    theVideo.setOutPoint(framesToSeconds(outPoint, FRAME_RATE));
+}
+
+function secondsToFrames(seconds, frameRate) {
+    frames = seconds * frameRate;
+    return Math.round(frames);
+}
+
+function framesToSeconds(frames, frameRate) {
+        seconds = frames / frameRate;
+        return  seconds;
+}
